70b5788c5020bcb0ef44f166deebbff84f48ce8a
Javi <Jjaviergonzalez23@gmail.com>
2026-05-02 00:40:04 +0200

    Fix: preview

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'https://rafaello3d.es/:path*',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
